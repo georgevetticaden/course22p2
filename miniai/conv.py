cf0d804a232a8d1458afe609fb038140c1727cc8
George Vetticaden <george.vetticaden@gmail.com>
2024-01-16 18:19:55 +0000

    finishing notebook 07 foundations

--- miniai/conv.py
+++ miniai/conv.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['def_device', 'conv', 'to_device', 'collate_device']
 
-# %% ../nbs/07_convolutions.ipynb 4
+# %% ../nbs/07_convolutions.ipynb 5
 import torch
 from torch import nn
 
@@ -13,13 +13,13 @@ from typing import Mapping
 from .training import *
 from .datasets import *
 
-# %% ../nbs/07_convolutions.ipynb 71
+# %% ../nbs/07_convolutions.ipynb 75
 def conv(ni, nf, ks=3, stride=2, act=True):
     res = nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2)
     if act: res = nn.Sequential(res, nn.ReLU())
     return res
 
-# %% ../nbs/07_convolutions.ipynb 76
+# %% ../nbs/07_convolutions.ipynb 80
 def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def to_device(x, device=def_device):
@@ -28,3 +28,5 @@ def to_device(x, device=def_device):
     return type(x)(to_device(o, device) for o in x)
 
 def collate_device(b): return to_device(default_collate(b))
+
+def_device
